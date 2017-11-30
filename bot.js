2e594ee524c7b0f5653263d633adf09ef80e837e
Sam Couch <sam@couch.rocks>
2017-11-30 00:41:59 -0500

    remove bloat from bot

--- bot.js
+++ bot.js
@@ -29,14 +29,6 @@ controller.on('facebook_optin', function(bot, message) {
     bot.reply(message, 'Thanks for checking up out!')
 })
 
-var attachment = {
-        'type':'image',
-        'payload':{
-            'url':'https://images.unsplash.com/photo-1456244440184-1d494704a505',
-            'is_reusable': true
-        }
-    };
-
 controller.hears(['.*'], 'message_received', function(bot, message) {
   console.log(message)
 
@@ -44,45 +36,5 @@ controller.hears(['.*'], 'message_received', function(bot, message) {
     bot.reply(message, 'I\'m sorry, but for technical reasons I can\'t respond to your message')
   } else {
     bot.reply(message, message.watsonData.output.text.join('\n'))
-
-    if ('object' in message.watsonData.context) {
-      if (message.watsonData.context.object) {
-        getPhoto(message.watsonData.context, (err, photo) => {
-          if (err) {
-            bot.reply('This is awkward... Something went wrong and I can\'t find a photo')
-          } else {
-            let attachment = {
-              'type':'image',
-              'payload':{
-                'url': photo,
-                'is_reusable': true
-              }
-            }
-
-            controller.api.attachment_upload.upload(attachment, (err, attachmentId) => {
-              if (!err) {
-                let image =  {
-                  'attachment': {
-                    'type': 'image',
-                    'payload': {
-                      'attachment_id': attachmentId
-                    }
-                  }
-                }
-                bot.reply(message, image)
-              }
-            })
-          }
-        })
-      }
-    }
   }
 })
-
-function getPhoto(data, cb) {
-  let main = data.object
-  console.log('HERE', main)
-  request.get(`https://source.unsplash.com/1600x900/?${main}`, function (err, res, body) {
-    cb(err, res.request.uri.href)
-  })
-}
